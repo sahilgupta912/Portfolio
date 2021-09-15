c6e3b266a3c19ac183e284ec487824622bae0e62
Sahil Gupta <sahilgupta912@gmail.com>
2021-09-16 04:41:25 +0530

    added scrollability

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const links=document.querySelectorAll(".navLink");
+
+links.forEach((item)=>{
+    item.addEventListener("click",()=>
+    {
+        let el=document.getElementById(item.getAttribute("data-link"));
+        el.scrollIntoView({behavior:"smooth",block:"start"});
+    })
+})
+
+const l=document.getElementById("logo");
+l.addEventListener("click",()=>
+{
+    let e=document.getElementById(l.getAttribute("data-link"));
+    e.scrollIntoView({behavior:"smooth",block:"start"});
+})
